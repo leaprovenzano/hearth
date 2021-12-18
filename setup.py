13a92a2efbfd9fd00efe1eeb09fa16a1c61b55bf
lea <leaprovenzano@gmail.com>
2021-12-18 12:24:00 +0000

    extend python version support and torch.1.10.1

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
     ],
     description="a collection tools and extensions for working with pytorch",
