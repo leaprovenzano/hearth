eab155e58708de0224aba5e0d7f662fdef82d43c
lea <leaprovenzano@gmail.com>
2022-01-30 07:24:28 +0000

    bumpversion to 0.4.0

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leaprovenzano/hearth',
-    version='0.3.0',
+    version='0.4.0',
     zip_safe=False,
 )
